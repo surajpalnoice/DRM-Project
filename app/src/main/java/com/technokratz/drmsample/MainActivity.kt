34ae1effe159d068a882fed7e84f758cf9be8727
Suraj <surajpal@noice.id>
2023-02-01 19:57:34 +0530

    Bitrate Switching implementation

--- app/src/main/java/com/technokratz/drmsample/MainActivity.kt
+++ app/src/main/java/com/technokratz/drmsample/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.technokratz.drmsample
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.util.MimeTypes
 import com.google.android.exoplayer2.util.Util
 import com.technokratz.drmsample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), Player.Listener {
+
+class MainActivity : AppCompatActivity(), Player.Listener, BandwidthMeter.EventListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var player: SimpleExoPlayer
@@ -17,7 +24,16 @@ class MainActivity : AppCompatActivity(), Player.Listener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        player = SimpleExoPlayer.Builder(this).build()
+
+        val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
+        bandwidthMeter.addEventListener(Handler(), this)
+
+        val videoTrackSelectionFactory: AdaptiveTrackSelection.Factory =
+            AdaptiveTrackSelection.Factory()
+        val trackSelector = DefaultTrackSelector(this, videoTrackSelectionFactory)
+
+        player = SimpleExoPlayer.Builder(this).setTrackSelector(trackSelector)
+            .setBandwidthMeter(bandwidthMeter).build()
     }
 
     private fun initializePlayer() {
@@ -37,10 +53,10 @@ class MainActivity : AppCompatActivity(), Player.Listener {
 
 
         val item = MediaItem.Builder()
-            .setUri("https://storage.googleapis.com/drm-pallycon/shubham-fmp4/ezdrm-test/sample_tj.m3u8")
-            //.setUri("https://storage.googleapis.com/drm-pallycon/shubham-fmp4/expressplay/sample_tj.m3u8")
+            //.setUri("https://storage.googleapis.com/drm-pallycon/shubham-fmp4/without-drm/sample_tj.m3u8")
+            .setUri("https://cdn.noiceid.cc/transcoded/content-audio-1674773324802.m3u8")
             .setMimeType(MimeTypes.APPLICATION_M3U8)
-            .setDrmConfiguration(drmConfiguration)
+            //.setDrmConfiguration(drmConfiguration)
             .build()
 
         player.setMediaItem(item)
@@ -82,5 +98,16 @@ class MainActivity : AppCompatActivity(), Player.Listener {
         if (Util.SDK_INT > 23) {
             releasePlayer()
         }
+    }
+
+    var bytes = 0L
+
+    override fun onBandwidthSample(elapsedMs: Int, bytesTransferred: Long, bitrateEstimate: Long) {
+        val bitrateInKB = bitrateEstimate * 0.000125
+        val bytesInKB = bytesTransferred * 0.001
+        val avgBitrate = (bytesTransferred * 8) / (elapsedMs / 1000)
+        Log.d("BitrateSwitching", "elapsedMs : $elapsedMs, bytesTransferred : $bytesInKB, bitrateEstimate : $bitrateInKB")
+        Log.d( "AverageBitrate", "Average bitrate (bps)= $avgBitrate");
+        bytes = bytesTransferred
     }
 }
